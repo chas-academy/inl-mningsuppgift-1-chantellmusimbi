21c4a5d00df70842c4ff867bd0a44f48a9d54742
mail@mail.com <chantellmusimbi@gmail.com>
2026-02-11 18:44:14 +0100

    Lämna in

--- src/script.js
+++ src/script.js
@@ -26,20 +26,21 @@ const contactBook = {
 };
   
   // Ange en grupp
-  let groupToShow;
+  let groupToShow = "familj";
   
 
+function showContacts(contactBook, groupToShow) {
+  // Rör ej denna kod
 
+// Loopar igenom alla kontakter och jämför grupp
+   for (contact of contactBook.contacts){
+    if (contact.group === groupToShow){
 
-function showContacts(contactBook, groupToShow) { // Rör ej denna kod
-
-
-
-    // Loopar igenom alla kontakter och jämför grupp
+    return `Namn: ${contact.name}, Telefon: ${contact.phone}`;
    
-    return  // här ska sträng med namn och tekefonnummer visas i form av string interpolation
-
-
+     // här ska sträng med namn och tekefonnummer visas i form av string interpolation
+  }
+}
 }
 
 
